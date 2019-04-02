ee3c2fceb0fac248be71678b2d51d866ceac7a9b
ujikit <fauzizaki15@gmail.com>
2019-04-02 17:22:51 +0700

    Adding popular list at Index View

--- App.js
+++ App.js
@@ -8,30 +8,28 @@
 
 import React, {Component} from 'react';
 import { ActivityIndicator, Image, StyleSheet, Text, View, ScrollView } from 'react-native';
-import { Container, Header, Item, Input, Icon, Button } from 'native-base';
-import { Card } from 'react-native-elements';
+import { Container, Header, Item, Input, Icon, Button, Card, CardItem, Body } from 'native-base';
 
 export default class App extends Component {
   render() {
 
     return (
-      <View>
-        <View>
-        <Header searchBar rounded noShadow style={{backgroundColor: "transparent"}}>
-          <Item>
-            <Icon name="ios-search" />
-            <Input placeholder="Search" />
-            <Icon name="ios-people" />
-          </Item>
-          <Button transparent>
-            <Text>Search</Text>
-          </Button>
-        </Header>
-        </View>
-        <View style={{paddingRight: 10, paddingLeft: 10}}>
-          <Image style={{width: '100%', resizeMode: 'contain', marginTop: -145}} source={require('./image1.jpg')} />
+        <View style={ styles.container }>
+          <Header searchBar rounded noShadow style={{backgroundColor: "transparent"}}>
+            <Item>
+              <Icon name="ios-search" />
+              <Input placeholder="Search" />
+              <Icon name="ios-people" />
+            </Item>
+            <Button transparent>
+              <Text>Search</Text>
+            </Button>
+          </Header>
+          <View style={ styles.top }>
+              <Image style={{width: '100%', resizeMode: 'contain'}} source={require('./image1.jpg')} />
+          </View>
+          <View style={ styles.center }>
 
-          <View style={{marginTop: -140}}>
             <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
               <View style={{width: 55, height: 20}}>
                 <Button light bordered style={{flexDirection: 'column', height: 58}}>
@@ -65,11 +63,22 @@ export default class App extends Component {
               </View>
             </View>
 
-
-
+          </View>
+          <View style={ styles.bottom }>
+            <View style={ styles.bottomItem }>
+              <View style={ styles.bottomItemInner }></View>
+            </View>
+            <View style={ styles.bottomItem }>
+              <View style={ styles.bottomItemInner }></View>
+            </View>
+            <View style={ styles.bottomItem }>
+              <View style={ styles.bottomItemInner }></View>
+            </View>
+            <View style={ styles.bottomItem }>
+              <View style={ styles.bottomItemInner }></View>
+            </View>
           </View>
         </View>
-      </View>
     );
 
   }
@@ -78,8 +87,40 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingRight: 10,
+    paddingLeft: 10,
   },
-});
+  top: {
+    height: '20%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
+  profileimage: {
+    width: '50%',
+    height: '50%',
+    borderRadius: 100,
+    borderWidth: 4,
+    borderColor: '#fff',
+  },
+  center: {
+    height: '10%',
+    backgroundColor: 'transparent',
+  },
+  bottom: {
+    height: '70%',
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 5,
+  },
+  bottomItem: {
+    width: '50%',
+    height: '50%',
+    padding: 5,
+  },
+  bottomItemInner: {
+    flex: 1,
+    backgroundColor: 'red',
+  }
+})
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### E-Commerce React Native
 
 - [x] Layouting Image Ads & Categories
+- [x] Layouting Popular List
+- [ ] ScrollView
 - [ ] Slider -> Image Ads
 - [ ] Slider -> Categories
